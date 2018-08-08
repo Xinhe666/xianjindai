73d84d340ae18a72f5feb7e1910966ec8c5e6fac
Xinhe666 <2286479828@qq.com>
2018-08-08 11:25:29 +0800

    first commit

---
+++ app/src/main/java/cashloan/xinhe/com/xianjindai/MainActivity.java
@@ -0,0 +1,13 @@
+package cashloan.xinhe.com.xianjindai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
